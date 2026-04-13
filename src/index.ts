ef53df646137408b052f0b377d895a276e3b359c
Kaynan <vitor.costa2@estudante.ifms.edu.br>
2026-04-13 14:39:32 -0400

    PE

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,139 @@
 import mysql from 'mysql2/promise';
 import express from 'express';
-cons
+const app = express()
+app.use(express.json())
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'aula1',
+});
+app.get("/listar_produtos", async (req, res) => {
+    try {
+        const [resultado, campos] =
+            await connection.execute(`SELECT * FROM produto`)
+        console.log(resultado)
+        res.status(200).json(resultado)
+    } catch (err) {
+        console.log(err);
+        if (err instanceof Error && 'code' in err && err.code === 'ECONNREFUSED') {
+            res.status(500).json({ mensagem: "Erro: Ligue o LARAGON!" })
+        }
+        else if (err instanceof Error && 'code' in err && err.code === 'ER_BAD_DB_ERROR') {
+            res.status(500).json({ mensagem: "Erro: Crie o banco de dados ou confira se o nome está correto!" })
+        }
+        else if (err instanceof Error && 'code' in err && err.code === 'ER_ACCESS_DENIED_ERROR') {
+            res.status(500).json({ mensagem: "Erro: Confira o Usuario e Senha da Conexão!" })
+        }
+        else if (err instanceof Error && 'code' in err && err.code === 'ER_NO_SUCH_TABLE') {
+            res.status(500).json({ mensagem: "Erro: Confira o nome da tabela no banco ou crie a tabela!" })
+        }
+        else if (err instanceof Error && 'code' in err && err.code === 'ER_PARSE_ERROR') {
+            res.status(500).json({ mensagem: "Erro: Confira o código SQL do EXECUTE!" })
+        }
+        else {
+            res.status(500).json({ mensagem: "Erro no servidor!" })
+        }
+    }
+})//listar
+app.post("/cadastro_produto", async (req, res) => {
+    try {
+        //const preparacao = await connection.prepare("select * from pessoa");
+        const {id,nome,categoria,preco,data_criacao,data_modificacao  } = req.body;
+        //Valide se o id e o nome foram passados corretamente. (Algum valor)
+        //Se não foram, retorne o código 400 com a mensagem "id ou nome inválidos"
+        //Não deixe o código executar a parte de baixo quando for inválido.
+
+        if (!id || !nome || !categoria || !preco || !data_criacao || !data_modificacao) {
+            return res.status(500).json({ mensagem: "id, nome, categoria, preco, data_criacao e data_modificacao são obrigatórios!" })
+            
+        }
+        const [resultado] =
+            await connection.execute(`insert into produto values (?,?,?,?,?,?)`, [id, nome, categoria, preco, data_criacao, data_modificacao])
+        console.log(resultado)
+        res.status(201).json({ mensagem: "Sucesso" })
+    } catch (err) {
+        console.log(err);
+        if (err instanceof Error && 'code' in err && err.code === 'ECONNREFUSED') {
+            res.status(500).json({ mensagem: "Erro: Ligue o LARAGON!" })
+        }
+        else if (err instanceof Error && 'code' in err && err.code === 'ER_BAD_DB_ERROR') {
+            res.status(500).json({ mensagem: "Erro: Crie o banco de dados ou confira se o nome está correto!" })
+        }
+        else if (err instanceof Error && 'code' in err && err.code === 'ER_ACCESS_DENIED_ERROR') {
+            res.status(500).json({ mensagem: "Erro: Confira o Usuario e Senha da Conexão!" })
+        }
+        else if (err instanceof Error && 'code' in err && err.code === 'ER_NO_SUCH_TABLE') {
+            res.status(500).json({ mensagem: "Erro: Confira o nome da tabela no banco ou crie a tabela!" })
+        }
+        else if (err instanceof Error && 'code' in err && err.code === 'ER_PARSE_ERROR') {
+            res.status(500).json({ mensagem: "Erro: Confira o código SQL do EXECUTE!" })
+        }
+        else {
+            res.status(500).json({ mensagem: "Erro no servidor!" })
+        }
+    }
+})//Inserir
+
+app.get("/listar_produtos_informatica", async (req, res) => {
+    try {
+        const [resultado, campos] =
+            await connection.execute(`SELECT * FROM produto WHERE categoria = 'informatica'`);
+        console.log(resultado)
+        res.status(200).json(resultado)
+    } catch (err) {
+        console.log(err);
+        if (err instanceof Error && 'code' in err && err.code === 'ECONNREFUSED') {
+            res.status(500).json({ mensagem: "Erro: Ligue o LARAGON!" })
+        }
+        else if (err instanceof Error && 'code' in err && err.code === 'ER_BAD_DB_ERROR') {
+            res.status(500).json({ mensagem: "Erro: Crie o banco de dados ou confira se o nome está correto!" })
+        }
+        else if (err instanceof Error && 'code' in err && err.code === 'ER_ACCESS_DENIED_ERROR') {
+            res.status(500).json({ mensagem: "Erro: Confira o Usuario e Senha da Conexão!" })
+        }
+        else if (err instanceof Error && 'code' in err && err.code === 'ER_NO_SUCH_TABLE') {
+            res.status(500).json({ mensagem: "Erro: Confira o nome da tabela no banco ou crie a tabela!" })
+        }
+        else if (err instanceof Error && 'code' in err && err.code === 'ER_PARSE_ERROR') {
+            res.status(500).json({ mensagem: "Erro: Confira o código SQL do EXECUTE!" })
+        }
+        else {
+            res.status(500).json({ mensagem: "Erro no servidor!" })
+        }
+    }
+})
+
+app.get("/listar_produtos_caros", async (req, res) => {
+    try {
+        const [resultado, campos] =
+            await connection.execute(`SELECT * FROM produto WHERE preco > 100`);
+        console.log(resultado)
+        res.status(200).json(resultado)
+    } catch (err) {
+        console.log(err);
+        if (err instanceof Error && 'code' in err && err.code === 'ECONNREFUSED') {
+            res.status(500).json({ mensagem: "Erro: Ligue o LARAGON!" })
+        }
+        else if (err instanceof Error && 'code' in err && err.code === 'ER_BAD_DB_ERROR') {
+            res.status(500).json({ mensagem: "Erro: Crie o banco de dados ou confira se o nome está correto!" })
+        }
+        else if (err instanceof Error && 'code' in err && err.code === 'ER_ACCESS_DENIED_ERROR') {
+            res.status(500).json({ mensagem: "Erro: Confira o Usuario e Senha da Conexão!" })
+        }
+        else if (err instanceof Error && 'code' in err && err.code === 'ER_NO_SUCH_TABLE') {
+            res.status(500).json({ mensagem: "Erro: Confira o nome da tabela no banco ou crie a tabela!" })
+        }
+        else if (err instanceof Error && 'code' in err && err.code === 'ER_PARSE_ERROR') {
+            res.status(500).json({ mensagem: "Erro: Confira o código SQL do EXECUTE!" })
+        }
+        else {
+            res.status(500).json({ mensagem: "Erro no servidor!" })
+        }
+    }
+})
+
+
+//Criar o servidor
+app.listen(8000, () => {
+    console.log("Servidor iniciado na porta 8000")
+})
